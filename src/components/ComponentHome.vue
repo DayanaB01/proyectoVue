8341c57738d57302591729d2b79710592193f4c0
Dayana Betancur <dahianab01@gmail.com>
2022-05-25 18:20:46 -0500

    Imagenes

--- src/components/ComponentHome.vue
+++ src/components/ComponentHome.vue
@@ -22,13 +22,24 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
+          <h5>Biografia</h5>
           <p>Originario de comunidad etnica asentueda en el occidente de Caldas, tiene 55 años de
             los cuales 45 han sido dedicados a la agricultura y la artesania, habilidades que cual heredo de sus
             ancestros. Luis en parte de su tiempo se de didica a ser alfarero, arte que lo ha ayudado
-            a adquirir ingresos economicos para el bien estar de su familia.
-            Comunicate con Luis y adquiere de los más hermosos productos de la alfareria colombiana.
-            3134543245-344335678.
+            a adquirir ingresos economicos para el bien estar de su familia.<br>
+            Comunicate con Luis y adquiere de los más hermosos productos de la alfareria colombiana.<br><br>
+            Contactos: 3134543245-344335678.
           </p>
+          <div class="start">
+          <img src="@/assets/LuisBarro_I.jpg" class="img-thumbnail" alt="vasija">
+          <p>Producto: Vasija <br>Tamaño: Mediana <br>Precio: $25<br> Cantidades: 3</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
+          <div class="end">
+          <img src="@/assets/LuisBarro_III.jpg" class="img-thumbnail" alt="matero">
+          <p>Producto: Matero <br>Tamaño: Pequeño <br>Precio: $30<br> Cantidades: 1</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
          </div>
         </div>
        </div>
@@ -55,13 +66,24 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
+          <h5>Biografia</h5>
           <p>Luisa es una joven tejedora que a pesar de su corta edad se a dedicado a tejer
             mochilas, las cuales vende en el mercado de su pueblo natal. De esta manera Luisa
             a contribuido en la economia de su hogar, adémas enseña el arte de tejer a los niños
             que viven en su pueblo.<br>
             Si deseas comprar una de estas hermosas mochilas, comunicate con Luisa y adquierelas
-            directamente: 321453356-54322346.
+            directamente. <br><br> Contactos: 321453356-54322346.
           </p>
+          <div class="start">
+          <img src="@/assets/mochilas_I.jpg" class="img-thumbnail" alt="mochila basica">
+          <p>Producto: Mochila básica <br>Tamaño: Mediana <br>Precio: $35<br> Cantidades: 1</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
+          <div class="end">
+          <img src="@/assets/mochilas_III.jpg" class="img-thumbnail" alt="mochila viajera">
+          <p>Producto: Mochila viajera<br>Tamaño: Grande <br>Precio: $40<br> Cantidades: 2</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
          </div>
         </div>
        </div>
@@ -88,12 +110,25 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
          </div>
          <div class="modal-body">
+          <h5>Biografia</h5>
+          <p>
           La artesana Lucero es una madre cabeza de familia, que gracias al arte que aprendio de su familia,
           ha podido vivir economicamente dando estudio a sus hijos, todos los accesorios que hace los vende
           en los eventos de artesania que se hacen cerca del pueblo donde reside y del cual es también
           oriunda.<br>
           Si deseas adquirir uno de sus productos y contribuir con la economia de Lucer, comunicate
-          con ella: 321546466-3146785443.
+          con ella.<br><br>Contactos: 321546466-3146785443.
+          </p>
+          <div class="start">
+          <img src="@/assets/okama_I.jpg" class="img-thumbnail" alt="okama con aretes">
+          <p>Producto: Kit Okama <br>Tamaño: Grande <br>Precio: $48<br> Cantidades: 1</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
+          <div class="end">
+          <img src="@/assets/okama_III.jpg" class="img-thumbnail" alt="okama">
+          <p>Producto: Okama<br>Tamaño: Mediana <br>Precio: $40<br> Cantidades: 1</p>
+          <button type="button" class="btn btn-primary">Comprar</button>
+          </div>
          </div>
         </div>
        </div>
@@ -137,5 +172,12 @@ export default {
   p{
     color:black;
     font-size: 1rem;
+  }
+  .start{
+    width: 200px;
+    float: inline-end;
+  }
+  .end{
+    width: 200px;
   }
 </style>
